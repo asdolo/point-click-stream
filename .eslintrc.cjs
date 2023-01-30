0738872ceb31f80c2c468a5bf49164dab8e53615
Lucas Raposeiras <raposeiras.lucas@gmail.com>
2023-01-29 22:16:23 -0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+		{
+			'files': ['*.ts', '*.tsx'],
+			rules: {
+				'react/react-in-jsx-scope': 'off'
+			}
+		}
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+};
